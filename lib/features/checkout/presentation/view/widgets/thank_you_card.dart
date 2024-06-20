d36d405453008d7ebf5163e99d0cc4d8b1de1cc3
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-06-20 18:23:52 +0300

    part2

--- lib/features/checkout/presentation/view/widgets/thank_you_card.dart
+++ lib/features/checkout/presentation/view/widgets/thank_you_card.dart
@@ -1,3 +1,4 @@
+import 'package:checkout_app/core/utiles/styles.dart';
 import 'package:flutter/material.dart';
 
 class ThankYouCard extends StatelessWidget {
@@ -8,12 +9,77 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: ShapeDecoration(
         color: const Color(0xffEDEDED),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
       ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 66,
+            ),
+            const Text(
+              'Thank you!',
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/24/2023',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:15 AM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Sam Louis',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentItemInfo extends StatelessWidget {
+  const PaymentItemInfo({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+  final String title, value;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: Styles.styleRegular18,
+        ),
+        Text(
+          value,
+          style: Styles.stylesemiBold18,
+        ),
+      ],
     );
   }
 }
