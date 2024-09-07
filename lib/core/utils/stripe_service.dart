8aa16be8b5c6c216246a81c1eb7386651fb72bf0
AmrMohamed97 <amr.elzorkany97@gmail.com>
2024-09-07 10:38:59 +0300

    edit

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,8 +1,9 @@
 import 'package:checkout_app/checkout/data/models/payment_intent_input_model.dart';
 import 'package:checkout_app/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+import 'package:checkout_app/core/keys.dart';
 import 'package:checkout_app/core/utils/api_services.dart';
 import 'package:dio/dio.dart';
-
+ 
 class StripeService {
   ApiServices apiServices = ApiServices();
   Future<PaymentIntentModel> createPaymentIntent(
@@ -10,8 +11,7 @@ class StripeService {
     Response response = await apiServices.post(
       url: "https://api.stripe.com/v1/payment_intents",
       body: paymentIntentInput.toJson(),
-      token:
-          "sk_test_51PvqsUHIpPyEiKw9pvlFUxZtCQAOOTpZdPWtiROU0QSkHvFpNc2MuZgoyeWKxR9QV8cWymNhmBkPzItCBqb2R2dL00HoBYlMqT",
+      token:Keys.secretKey,
     );
     PaymentIntentModel paymentIntentInputModel =
         PaymentIntentModel.fromJson(response.data);
